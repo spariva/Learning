1dc1fb8036d54cf379bad0efa8fe2d818af5bc37
Maki <110998002+spariva@users.noreply.github.com>
2023-01-17 15:22:27 +0100

    Added two arrays exercises
    I didn´t fix yet Arrays2, where I couldn´t figure out why I´m adding twice the last row and file of the bidimensional array.

---
+++ Arrays1.java
@@ -0,0 +1,36 @@
+
+/*Define tres arrays de 20 números enteros cada uno, con nombres número, cuadrado
+ y cubo.
+ Carga el array numero con valores aleatorios entre 0 y 100. En el array
+ cuadrado se deben almacenar los cuadrados de los valores que hay en el array
+ número.
+ En el array cubo se deben almacenar los cubos de los valores que hay en
+ numero. A continuación, muestra el contenido de los tres arrays dispuesto en tres
+ columnas.*/
+
+public class Arrays1 {
+    public static void main(String[] args) {
+        int[] number = new int[20];
+        int[] square = new int[20];
+        int[] cube = new int[20];
+
+        for (int i = 0; i < cube.length; i++) {
+            number[i]=(int)(Math.random()*100+1);
+            square[i]=(int) Math.pow(number[i],2);
+            cube[i]=(int)Math.pow(number[i],3);
+        }
+
+        /* No se podría imprimir en un array unidimensional y cambiar columnas por filas. Porque imprime de izq a derecha la consola. (transposición array y giras el cubo).
+        for (int i = 0; i < cube.length; i++) {
+            for (int j = 0; j < cube.length; j++) {
+                System.out.print(number[j]+"   ");
+            }
+            System.out.println();
+        }*/
+
+    for (int i = 0; i < cube.length; i++) {
+        System.out.printf("1. %7d   2. %7d   3. %7d%n", number[i], square[i], cube[i]);
+    }
+    }
+    
+}
---
+++ Arrays2.java
@@ -0,0 +1,38 @@
+public class Arrays2{
+    public static void main(String[] args) {
+                /*Escribe un programa que genere 20 números aleatorios entre 1 y 100. Estos
+        números se deben introducir en un array de dos dimensiones, de 4 filas por
+        5 columnas. El programa mostrará las sumas parciales de filas y columnas
+        igual que si de una hoja de cálculo se tratara. La suma total debe aparecer
+        en la esquina inferior derecha. 
+
+        Ejemplo salida:
+        1   2  3  4  5 | 15
+        6   7  8  9 10 | 40
+        11 12 13 14 15 | 65
+        16 17 18 19 20 | 90
+        ___________________
+        34 38 43 46 50 | 210 "pretty comments" - pretty woman (cuando pongas el comentario que suene la banda sonora de la peli)*/
+
+        int [][] array2 = new int [5][6];
+
+        for (int i = 0, a=0; i < array2.length; i++, a++) {
+            for (int j = 0, b=0; j < array2[i].length; j++, b++){
+                if(i != 4 && j != 5){
+                    array2[i][j] = (int)(Math.random()*100+1); //para que no cargue en la casilla de suma total.
+                }
+                if(a != 4 || b != 5){
+                    array2[i][5] += array2[a][b];
+                    array2[4][j] += array2[i][j];
+                    array2[4][5] += array2[i][j];
+                } //no entiendo por qué me suma el doble...
+            }
+        }
+
+        ev1.Metodos.mostrarArray("", array2, 6);
+        
+
+
+
+}
+}
