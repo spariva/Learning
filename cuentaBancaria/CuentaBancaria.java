5bd93b305c471e53d589e0402b66ffc232720894
Maki <makspariva@gmail.com>
2023-01-30 16:39:01 +0100

    What?
    I already merged, wtf

--- cuentaBancaria/CuentaBancaria.java
+++ cuentaBancaria/CuentaBancaria.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream:cuentaBancaria/CuentaBancaria.java
 package cuentaBancaria;
 //import java.lang.reflect.Method;
 import java.util.Scanner;
@@ -105,4 +106,114 @@ Métodos:
 - depositarDinero(???)
 - hacerTransferencia(???)
 - Otros métodos que se puedan necesitar
+=======
+import java.util.Scanner;
+
+public class CuentaBancaria{
+    private String numeroCuenta;
+    private String titular;
+    private double saldo;
+    private String moneda;
+    private static int contador; 
+    Scanner sc = new Scanner(System.in);
+//constructor
+    public CuentaBancaria(String numeroCuenta, String titular, int saldo, String moneda){
+        this.numeroCuenta = numeroCuenta;
+        this.titular = titular;
+        this.saldo = 0;
+        this.moneda = moneda;
+        contador = +1;
+    }
+
+//
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public String getNombre() {
+        return titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setNombre(String titular) {
+        this.titular = titular;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    public void checkStatusAccount() {
+        System.out.println("Titular: " + titular + "con número de cuenta: " + numeroCuenta + 
+        " y un saldo de: " + saldo + moneda);
+    }
+    public boolean retirarDinero(double dineroRetirado) {
+        if(dineroRetirado > saldo){
+            return false;
+        } else {
+            saldo -= dineroRetirado;
+            return true;
+        }
+    }
+    public void depositarDinero(double dineroDepositado) {
+        saldo += dineroDepositado;
+        System.out.println("Nuevo balance: "+ saldo + moneda);
+    }
+    public boolean hacerTransferencia(double dineroTransferido, CuentaBancaria cuentaDestino) { 
+        if(dineroTransferido > saldo){
+            return false;
+        } else {
+            saldo -= dineroTransferido;
+            cuentaDestino.saldo += dineroTransferido; //puedo, o tengo que usar su set que es público?
+            return true;
+        }
+  
+    }
+    public void setTitular(){
+        titular = sc.nextLine();
+    }
+    public String setNumeroCuenta(){
+        numeroCuenta = String.format("%020d", contador);
+        return numeroCuenta;
+
+    }
+    /*protected String setNumeroCuenta(){
+        for (int i = 0; i < numeroCuenta.length(); i++) {
+            numeroCuenta = new StringBuilder(numeroCuenta).append(String.valueOf(i)).toString();
+        }
+
+    }
+*/
+}
+    /*
+    public int[] generarNumeroCuenta(){
+        for (int i; i < numeroCuenta.length; i++){
+                        
+        }
+    }
+
+
+}
+/*Crea la clase CuentaBancaria
+
+Sus atributos serán todos privados.
+
+Atributos:
+- Número de cuenta (20 dígitos)
+- Titular
+- Saldo
+- Moneda
+
+Los métodos getter y setter se crearán solamente cuando sean necesarios:
+-si no necesitas extraer el valor de un atributo, no hace falta crear el getter
+-si no necesitas asignarle valor a un atributo, no hace falta crear el setter
+
+Métodos:
+- retirarDinero(???)
+- depositarDinero(???)
+- hacerTransferencia(???)
+- Otros métodos que se puedan necesitar
+>>>>>>> Stashed changes:CuentaBancaria.java
  */
