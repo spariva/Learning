9d9058a8d5a912ad05f8982a420904dc5322f2f9
Maki <makspariva@gmail.com>
2023-02-27 20:36:32 +0100

    Start
    Check and pattern missing.

--- mineSeeker/src/Board.java
+++ mineSeeker/src/Board.java
@@ -1,24 +1,21 @@
 package mineSeeker.src;
 
 public class Board {
-    private Cell[][] board;
-    private Cell bombPosition1;
+    private int[][] board;
+    private int bombPosition1;
     private int minesAmount = 3;
 
     public Board() {
-        board = new Cell[6][6];
-        randomBombs(this.board);
-        
-    }
-
-    public Board randomBombs(Board board) {
-        for (int i = 0; i < this.minesAmount; i++) {
-            bombPosition1 = new Cell();
-            board[(int)(Math.random()*7)][(int)(Math.random()*7)] = bombPosition1;
-            bombPosition1.setBomb(true);
+        board = new int[6][6];
+        for (int i = 0; i < minesAmount; i++) {
+            randomBombs();
         }
-        return board;
     }
+    public int randomBombs() {
+        bombPosition1 = (int)(Math.random()*101);
+        board[(int)(Math.random()*7)][(int)(Math.random()*7)] = bombPosition1;
+        return bombPosition1;
+    } 
     /*    private Cell[][] board;
     private Cell bombPosition1;
     private int minesAmount = 3;
@@ -29,7 +26,6 @@ public class Board {
             randomBombs();
         }
     }
-
     public Cell randomBombs() {
         bombPosition1 = new Cell();
         board[(int)(Math.random()*7)][(int)(Math.random()*7)] = bombPosition1;
@@ -37,20 +33,5 @@ public class Board {
         bombPosition1.setBomb(true);
 
         return bombPosition1;
-    } */
-
-
-
-
-
-    
-    
-
-
-    
-    
-    
-    
-
-
+    }  */
 }
