a4c24008769573165ee61d7b2236c3fa18caeacf
Maki <makspariva@gmail.com>
2023-05-28 07:26:47 +0200

    Procedures finished.

--- webPLSQL/Código SQL/gpt.sql
+++ webPLSQL/Código SQL/gpt.sql
@@ -30,8 +30,69 @@ EXCEPTION
         DBMS_OUTPUT.PUT_LINE('El producto con el código ' || p_nombre_producto || ' no tiene suficiente stock.');
 END;
 /
+
+DECLARE
+  v_producto VARCHAR2(100) := 'HEURA';
+  v_cantidad NUMBER := 10; 
+  v_resultado BOOLEAN;
+BEGIN
+  v_resultado := VerificarStock(v_producto, v_cantidad);
+  
+  IF v_resultado THEN
+    DBMS_OUTPUT.PUT_LINE('El producto ' || v_producto || ' tiene suficiente stock.');
+  ELSE
+    DBMS_OUTPUT.PUT_LINE('El producto ' || v_producto || ' no tiene suficiente stock.');
+  END IF;
+END;
+/ 
+
+
 --A continuación, vamos a crear un procedimiento llamado AddProductoPedido que utilizará la función VerificarStock para verificar el stock antes de realizar un pedido. El procedimiento aceptará el código de producto y la cantidad solicitada como parámetros, y mostrará un mensaje de aviso si no hay suficiente stock:
-CREATE OR REPLACE FUNCTION AddProductoPedido(p_nombre_producto VARCHAR2, p_cantidad NUMBER)
+CREATE OR REPLACE FUNCTION AddProductoPedido(p_num_pedido VARCHAR2, p_nombre_producto VARCHAR2, p_cantidad NUMBER)
+RETURN NUMBER
+IS
+   v_stock_actual NUMBER;
+   Sin_stock_Exception EXCEPTION;
+   v_precio NUMBER;
+   v_num_linea VARCHAR2(2);
+   v_num_linea_number NUMBER;
+BEGIN
+   SELECT PRECIO INTO v_precio
+      FROM PRODUCTOS
+      WHERE NOMBRE_P = p_nombre_producto;
+
+   IF VerificarStock(p_nombre_producto, p_cantidad) THEN
+      SELECT NVL(MAX(SUBSTR(NUM_LINEA, 2)), 0) + 1 INTO v_num_linea_number
+         FROM LINEA_PEDIDO
+         WHERE NUM_PEDIDO = p_num_pedido;
+
+      v_num_linea := 'P' || TO_CHAR(v_num_linea_number);
+      
+      INSERT INTO LINEA_PEDIDO (NUM_LINEA, CANTIDAD, NUM_PEDIDO, COD_PRODUCTO)
+         VALUES (v_num_linea, p_cantidad, p_num_pedido, p_nombre_producto);   
+      COMMIT;
+      
+      DBMS_OUTPUT.PUT_LINE('¡Pedido realizado exitosamente!');
+      RETURN v_precio * p_cantidad;
+   ELSE
+      SELECT STOCK INTO v_stock_actual
+         FROM PRODUCTOS
+         WHERE NOMBRE_P = p_nombre_producto;
+      RAISE Sin_stock_Exception;
+      RETURN 0;
+   END IF;
+
+EXCEPTION
+   WHEN Sin_stock_Exception THEN
+      DBMS_OUTPUT.PUT_LINE('No hay suficiente stock para realizar el pedido. Stock actual: ' || v_stock_actual);
+      RETURN 0;
+END;
+/
+
+
+
+
+CREATE OR REPLACE FUNCTION AddProductoPedido(p_num_pedido VARCHAR2, p_nombre_producto VARCHAR2, p_cantidad NUMBER)
 RETURN NUMBER
 IS
    v_stock_actual NUMBER;
@@ -43,8 +104,6 @@ BEGIN
       WHERE NOMBRE_P = p_nombre_producto;
 
    IF VerificarStock(p_nombre_producto, p_cantidad) THEN
-      INSERT INTO CABECERA_PEDIDO 
-         VALUES (p_num_pedido, p_dni, SYSDATE);
       INSERT INTO LINEA_PEDIDO
          VALUES (p_num_pedido, 1, p_nombre_producto, p_cantidad);
       
@@ -65,6 +124,50 @@ EXCEPTION
       DBMS_OUTPUT.PUT_LINE('No hay suficiente stock para realizar el pedido.'|| v_stock_actual);
 END;
 /
+
+
+
+CREATE OR REPLACE PROCEDURE AddProductoPedido(p_num_pedido VARCHAR2, p_nombre_producto VARCHAR2, p_cantidad NUMBER)
+IS
+   v_stock_actual NUMBER;
+   Sin_stock_Exception EXCEPTION;
+   v_precio NUMBER;
+   v_num_linea NUMBER;
+BEGIN
+   SELECT PRECIO INTO v_precio
+      FROM PRODUCTOS
+      WHERE NOMBRE_P = p_nombre_producto;
+
+   IF VerificarStock(p_nombre_producto, p_cantidad) THEN
+      SELECT NVL(MAX(NUM_LINEA), 0) + 1 INTO v_num_linea
+         FROM LINEA_PEDIDO
+         WHERE NUM_PEDIDO = p_num_pedido;
+
+      INSERT INTO LINEA_PEDIDO (NUM_PEDIDO, NUM_LINEA, COD_PRODUCTO, CANTIDAD)
+         VALUES (p_num_pedido, v_num_linea, p_nombre_producto, p_cantidad);
+      
+      UPDATE CABECERA_PEDIDO
+      SET FECHA = SYSDATE
+      WHERE NUM_PEDIDO = p_num_pedido;
+      
+      COMMIT;
+      
+      DBMS_OUTPUT.PUT_LINE('¡Pedido realizado exitosamente!');
+      DBMS_OUTPUT.PUT_LINE('Precio total: ' || v_precio * p_cantidad);
+   ELSE
+      SELECT STOCK INTO v_stock_actual
+         FROM PRODUCTOS
+         WHERE NOMBRE_P = p_nombre_producto;
+      RAISE Sin_stock_Exception;
+   END IF;
+
+EXCEPTION
+   WHEN Sin_stock_Exception THEN
+      DBMS_OUTPUT.PUT_LINE('No hay suficiente stock para realizar el pedido. Stock actual: ' || v_stock_actual);
+END;
+/
+
+
 
 CREATE OR REPLACE FUNCTION AddProductoPedido(p_nombre_producto VARCHAR2, p_cantidad NUMBER, descuento NUMBER)
 RETURN NUMBER
@@ -226,7 +329,7 @@ END;
 /
 EXECUTE BORRAR_ASIGNATURAS;
 
---trigger control de conexiones
+-- trigger control de conexiones 777
 CREATE TABLE CONTROL_CONEXIONES(
     USUARIO VARCHAR2(20),
     MOMENTO TIMESTAMP,
@@ -392,22 +495,22 @@ BEGIN
     v_codigo_producto_decimal := BinarioADecimal(v_codigo_producto_binario);
     SELECT STOCK INTO v_stock_actual
     FROM PRODUCTOS
-    WHERE CODIGO = v_codigo_producto_decimal;
+    WHERE CODIGO = v_codigo_producto_binario;
     
       IF v_codigo_producto_decimal > 255 THEN
          RAISE codigo_incorrecto; --8 bits encendidos son 255
       ELSIF v_codigo_producto_decimal > 8 THEN
          UPDATE PRODUCTOS
          SET STOCK = v_stock_actual + 20
-         WHERE CODIGO = v_codigo_producto_decimal;
+         WHERE CODIGO = v_codigo_producto_binario;
       ELSE
          UPDATE PRODUCTOS
          SET STOCK = v_stock_actual + 10
-         WHERE CODIGO = v_codigo_producto_decimal;
+         WHERE CODIGO = v_codigo_producto_binario;
       END IF;
-    COMMIT;
     FETCH c_productos INTO v_codigo_producto_binario;
   END LOOP;
+   COMMIT;
   CLOSE c_productos;
   
   DBMS_OUTPUT.PUT_LINE('Stock actualizado exitosamente.');
@@ -436,3 +539,44 @@ BEGIN
 END;
 /
 
+CREATE OR REPLACE PROCEDURE ActualizarStock IS
+  CURSOR c_productos IS
+    SELECT CODIGO FROM PRODUCTOS;
+  v_codigo_producto_binario PRODUCTOS.CODIGO%TYPE;
+  v_codigo_producto_decimal NUMBER;
+  v_stock_actual PRODUCTOS.STOCK%TYPE;
+  codigo_incorrecto EXCEPTION;
+BEGIN
+  OPEN c_productos;
+  FETCH c_productos INTO v_codigo_producto_binario;
+
+  WHILE c_productos%FOUND LOOP
+    v_codigo_producto_decimal := BinarioADecimal(v_codigo_producto_binario);
+    SELECT STOCK INTO v_stock_actual
+    FROM PRODUCTOS
+    WHERE CODIGO = v_codigo_producto_decimal;
+    
+      IF v_codigo_producto_decimal > 255 THEN
+         RAISE codigo_incorrecto; --8 bits encendidos son 255
+      ELSIF v_codigo_producto_decimal > 8 THEN
+         UPDATE PRODUCTOS
+         SET STOCK = v_stock_actual + 20
+         WHERE CODIGO = v_codigo_producto_decimal;
+      ELSE
+         UPDATE PRODUCTOS
+         SET STOCK = v_stock_actual + 10
+         WHERE CODIGO = v_codigo_producto_decimal;
+      END IF;
+    FETCH c_productos INTO v_codigo_producto_binario;
+  END LOOP;
+   COMMIT;
+  CLOSE c_productos;
+  
+  DBMS_OUTPUT.PUT_LINE('Stock actualizado exitosamente.');
+EXCEPTION
+   WHEN codigo_incorrecto THEN
+    DBMS_OUTPUT.PUT_LINE('Error al actualizar stock: 8 bits a 1 es 255 max');
+   WHEN OTHERS THEN
+    DBMS_OUTPUT.PUT_LINE('Error al actualizar el stock: ' || SQLERRM);
+END;
+/
